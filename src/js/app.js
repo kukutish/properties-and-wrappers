52612466b794d5b9c56b3b5c4d0c5c8cf294b151
Sergey Manchenko <vekmvekz@mail.ru>
2023-05-03 15:41:29 +0400

    done

---
+++ src/js/app.js
@@ -0,0 +1,30 @@
+function orderByProps(obj, arr) {
+  const object = Object.assign(obj);
+  const result = [];
+  arr.forEach((elem) => {
+    for (const key in object) {
+      if (elem === key) {
+        result.push({ key: `${key}`, value: `${object[key]}` });
+        delete object[key];
+      }
+    }
+  });
+  const sortName = [];
+  for (const key in object) {
+    sortName.push({ key: `${key}`, value: `${object[key]}` });
+  }
+  sortName.sort((a, b) => a.key > b.key ? 1 : -1);
+  return [...result, ...sortName];
+}
+
+const obj = {
+  name: 'мечник',
+  health: 10,
+  level: 2,
+  attack: 80,
+  defence: 40,
+};
+
+console.log(orderByProps(obj, ["level", "health"]));
+
+export default orderByProps
